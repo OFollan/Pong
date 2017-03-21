21b6fe9d7b9fdd07b462bd81af5cb9d1adb34722
OFollan <ofollan@gmail.com>
2017-03-21 14:30:45 +0100

    Wooohooo, fixed stupid ball ++

--- Pong/empty-example/pong.js
+++ Pong/empty-example/pong.js
@@ -1,56 +1,73 @@
-var CanvasWidth = 400;
-var CanvasHeight = 300;
+var CanvasWidth = 700;
+var CanvasHeight = 400;
 var PaddleHeight = 100;
 var PaddleWidth = 10;
 var gameBallHeight = 10;
-var Speed = 4;
-var ballSpeed = 2;
-var LP;
-var RP;
-var gameBall;
-var button;
+var ballSpeed = 3;
+var LeftPaddle, RightPaddle, GameBall, button;
+var gameBallInCourt = false;
+var LeftScore = 0,
+    RightScore = 0;
+var speedSlider;
+var Speed = 2;
 
 function setup() {
     createCanvas(CanvasWidth, CanvasHeight)
     button = createButton("Play");
-    button.mousePressed(initializeGame);
+    button.mousePressed(resetGame);
+    speedSlider = createSlider(1,9,2);
+}
+
+function resetGame() {
+    gameBallInCourt = false;
+    initializeGame();
 }
 
 function initializeGame() {
-    LP = new Paddle(0);
-    RP = new Paddle(CanvasWidth - PaddleWidth);
-    gameBall = new gameBall();
+    LeftPaddle = new Paddle(0);
+    RightPaddle = new Paddle(CanvasWidth - PaddleWidth);
+    GameBall = new gameBall();
 }
 
 function draw() {
     background(170);
-    if (gameBall.gameBallInCourt == true) {
+    Speed = speedSlider.value();
+    drawScore();
+    if (gameBallInCourt == true) {
         if (keyIsPressed == true) {
             keyPressed();
         }
-        gameBall.Update();
-        LP.Update();
-        RP.Update();
-        gameBall.Show();
-        LP.Show();
-        RP.Show();
+        LeftPaddle.Update();
+        RightPaddle.Update();
+        GameBall.Update();
+        LeftPaddle.Show();
+        RightPaddle.Show();
+        GameBall.Show();
     } else {
-        textSize(30);
-        text("Press Play to play!", 10, 30);
         fill(0);
+        textSize(30);
+        text("Q - Z", 10, 30);
+        text("Up - Down", 550, 30);
     }
+}
+
+function drawScore() {
+    textSize(40);
+    fill(0);
+    var s = LeftScore.toString() + ' - ' + RightScore.toString();
+    text(s, CanvasWidth / 2 - 50, 30);
 }
 
 function keyPressed() {
     if (keyIsDown(DOWN_ARROW)) {
-        RP.ySpeed = Speed;
+        RightPaddle.ySpeed = Speed;
     } else if (keyIsDown(UP_ARROW)) {
-        RP.ySpeed = -Speed;
+        RightPaddle.ySpeed = -Speed;
     }
-    if (keyIsDown(90)) {
-        LP.ySpeed = Speed;
-    } else if (keyIsDown(81)) {
-        LP.ySpeed = -Speed;
+    if (keyIsDown(90)) { //Key: Z
+        LeftPaddle.ySpeed = Speed;
+    } else if (keyIsDown(81)) { //Key: Q
+        LeftPaddle.ySpeed = -Speed;
     }
 }
 
@@ -78,36 +95,38 @@ function Paddle(location) {
 }
 
 function gameBall() {
+    gameBallInCourt = true;
     this.x = CanvasWidth / 2;
     this.y = (CanvasHeight) / 2;
-    this.ySpeed = ballSpeed;
+    this.ySpeed = 0;
     this.xSpeed = -ballSpeed;
-    this.gameBallInCourt = true;
     this.Update = function() {
         if (this.CollideWithWalls()) {
             this.ySpeed = -this.ySpeed;
         } else if (this.CollideWithPaddle()) {
             this.changeSpeed();
         } else if (this.OutOfBounds()) {
-            this.gameBallInCourt = false;
+            gameBallInCourt = false;
         }
         this.x = this.x + this.xSpeed;
         this.y = this.y + this.ySpeed;
     }
     this.changeSpeed = function() {
         if (this.CollideLeft()) {
-            this.ySpeed = this.ySpeed - (LP.y + PaddleHeight / 2 - this.y) / 20;
+            this.ySpeed = this.ySpeed - (LeftPaddle.y + PaddleHeight / 2 - this.y) / 20;
             this.xSpeed = -this.xSpeed;
         }
         if (this.CollideRight()) {
-            this.ySpeed = this.ySpeed - (RP.y + PaddleHeight / 2 - this.y) / 20;
+            this.ySpeed = this.ySpeed - (RightPaddle.y + PaddleHeight / 2 - this.y) / 20;
             this.xSpeed = -this.xSpeed;
         }
     }
     this.OutOfBounds = function() {
         if (this.x > (CanvasWidth)) {
+            LeftScore++;
             return true;
         } else if (this.x < 0 - gameBallHeight) {
+            RightScore++;
             return true;
         }
         return false;
@@ -124,12 +143,12 @@ function gameBall() {
     }
     this.CollideLeft = function() {
         if (this.x - gameBallHeight == 0 || this.x - gameBallHeight == 1) {
-            return (this.y >= LP.y && this.y <= LP.y + PaddleHeight)
+            return (this.y >= LeftPaddle.y && this.y <= LeftPaddle.y + PaddleHeight)
         }
     }
     this.CollideRight = function() {
         if (this.x == (CanvasWidth - gameBallHeight - PaddleWidth) || this.x == (CanvasWidth - gameBallHeight - PaddleWidth) + 1) {
-            return (this.y >= RP.y && this.y <= RP.y + PaddleHeight)
+            return (this.y >= RightPaddle.y && this.y <= RightPaddle.y + PaddleHeight)
         }
     }
     this.Show = function() {
