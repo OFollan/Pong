9506cb0aa15ceb6b60efb5f3a441b960c8464e31
OFollan <ofollan@gmail.com>
2017-03-23 11:50:23 +0100

    hello

--- Pong/empty-example/server.js
+++ Pong/empty-example/server.js
@@ -7,11 +7,11 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('public', __dirname + '/public');
-// app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
-// app.get('/', function(request, response) {
-//   response.render('public/index');
-// });
+app.get('/', function(request, response) {
+  response.render('public/indexTest');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
